9d514df438e003df7b10a230172b44d2eb253607
soul <j983581@gmail.com>
2024-09-26 18:23:23 +0530

    all completed

--- AnkitSite/urls.py
+++ AnkitSite/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path,include
-from .views import index,project,blogs
+from .views import index,project,blogs,project_click
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('',index,name="home"),
     path('project/',project,name="project"),
     path('blog/',blogs,name="blogs"),
+    path('project/<int:project_id>/click/', project_click, name='project_click'),
 ]
 
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- AnkitSite/views.py
+++ AnkitSite/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,get_object_or_404, redirect
+from .models import Project,Category
 
-# Create your views here.
+# Home page
 def index(request):
-    return render(request,'main/index.html')
+    projects = Project.objects.all()[:2]
+    data={
+        'projects':projects,
+        # 'filters':category,
+    }
+    return render(request,'main/index.html',data)
 
+# Projects page
 def project(request):
-    return render(request,'main/Project.html')
+    projects = Project.objects.all().order_by('created_at')
+    category = Category.objects.all()
+    data={
+        'projects':projects,
+        'filters':category,
+    }
+    print(data)
+    return render(request,'main/Project.html',data)
 
+# blog page
 def blogs(request):
     return render(request,'main/blog.html')
+
+# Click counter
+def project_click(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    project.click_count += 1
+    project.save()
+    # Redirect to the project link
+    return redirect(project.project_link)
